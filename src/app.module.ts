faf1a61f330fe0a6a99a9c456de5ed73943cc6c8
DeonB <DeonButler44@gmail.com>
2022-07-15 10:34:47 -0400

    refined dc.yaml, removed unused components from app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
 import { TasksModule } from './tasks/tasks.module';
 import { DatabaseModule } from './database/database.module';
 
